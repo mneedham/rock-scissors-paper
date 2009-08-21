89523e763212a8d722bddbc5b06a1086e3179855
unknown <Administrator@.(none)>
2009-08-21 23:40:37 +0100

    keep getting caught by this non updating of original file mistake damnit!

--- RockScissorsPaper/RockScissorsPaperTest.cs
+++ RockScissorsPaper/RockScissorsPaperTest.cs
@@ -81,18 +81,6 @@ namespace RockScissorsPaper
     }
 
     [TestFixture]
-    public class WeaponComparisonTest
-    {
-        [Test]
-        public void PaperShouldBeRecognisedAsBeingDifferentToOtherWeapons()
-        {
-            Assert.AreEqual(true, new WeaponComparison(new Paper(), new Rock()).AreDifferent());
-            Assert.AreEqual(true, new WeaponComparison(new Paper(), new Scissors()).AreDifferent());
-            Assert.AreEqual(false, new WeaponComparison(new Paper(), new Paper()).AreDifferent());
-        }
-    }
-
-    [TestFixture]
     public class WeaponOfMassDestructionTest
     {
 
