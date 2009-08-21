3fef0dc135b2af533ce7855870a49a5f8ba5b220
unknown <Administrator@.(none)>
2009-08-21 20:23:04 +0100

    and now to another file

---
+++ RockScissorsPaper/Rock.cs
@@ -0,0 +1,20 @@
+namespace RockScissorsPaper
+{
+    public class Rock : RockScissorsPaperTest.WeaponOfMassDestruction
+    {
+        public bool BeatsPaper()
+        {
+            return false;
+        }
+
+        public bool BeatsRock()
+        {
+            return false;
+        }
+
+        public bool BeatsScissors()
+        {
+            return false;
+        }
+    }
+}
--- RockScissorsPaper/RockScissorsPaperTest.cs
+++ RockScissorsPaper/RockScissorsPaperTest.cs
@@ -68,22 +68,4 @@ namespace RockScissorsPaper
 
        
     }
-
-    public class Rock : RockScissorsPaperTest.WeaponOfMassDestruction
-    {
-        public bool BeatsPaper()
-        {
-            return false;
-        }
-
-        public bool BeatsRock()
-        {
-            return false;
-        }
-
-        public bool BeatsScissors()
-        {
-            return false;
-        }
-    }
 }
