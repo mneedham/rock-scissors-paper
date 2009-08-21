2e662db306b761c567677fc8045b39a3b69c3910
unknown <Administrator@.(none)>
2009-08-22 00:01:58 +0100

    extracted base class - seems reasonable

--- RockScissorsPaper/Scissors.cs
+++ RockScissorsPaper/Scissors.cs
@@ -1,29 +1,49 @@
+using System;
+
 namespace RockScissorsPaper
 {
-    public class Scissors : IWeaponOfMassDestruction
+    public abstract class WeaponBase : IWeaponOfMassDestruction
     {
-        public bool BeatsPaper()
+        private readonly Func<IWeaponOfMassDestruction, bool> beatenBy;
+
+        protected WeaponBase(Func<IWeaponOfMassDestruction, bool> beatenBy)
         {
-            return true;
+            this.beatenBy = beatenBy;
         }
 
-        public bool BeatsScissors()
-        {
-            return false;
-        }
-
-        public bool BeatsRock()
-        {
-            return false;
-        }
+        public abstract bool BeatsPaper();
+        public abstract bool BeatsScissors();
+        public abstract bool BeatsRock();
 
         public string Battling(IWeaponOfMassDestruction weapon)
         {
             if (new WeaponComparison(this, weapon).AreDifferent())
             {
-                return weapon.BeatsScissors() ? "loss" : "win";
+                return beatenBy(weapon) ? "loss" : "win";
             }
             return "draw";
         }
     }
+
+    public class Scissors : WeaponBase
+    {
+        public Scissors() : base(weapon => weapon.BeatsScissors()) { }
+
+        public override bool BeatsPaper()
+        {
+            return true;
+        }
+
+        public override bool BeatsScissors()
+        {
+            return false;
+        }
+
+        public override bool BeatsRock()
+        {
+            return false;
+        }
+    }
+
+ 
 }
