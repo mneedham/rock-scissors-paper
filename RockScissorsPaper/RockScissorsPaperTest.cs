aa7f6573feb24344132c8c5d52af7d617b2d7d11
unknown <Administrator@.(none)>
2009-08-21 19:50:08 +0100

    paper should beat rock extracted to method

--- RockScissorsPaper/RockScissorsPaperTest.cs
+++ RockScissorsPaper/RockScissorsPaperTest.cs
@@ -2,12 +2,18 @@
 
 namespace RockScissorsPaper
 {
+    [TestFixture]
     public class RockScissorsPaperTest
     {
         [Test]
         public void PaperShouldBeatRock()
         {
-            
+            Assert.AreEqual(true, PaperBeatsRock());
+        }
+
+        private bool PaperBeatsRock()
+        {
+            return true;
         }
     }
 }
