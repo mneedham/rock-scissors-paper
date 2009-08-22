b0194f5fdb89dc5b8837a19efc93a906f3fb7783
unknown <Administrator@.(none)>
2009-08-22 02:55:19 +0100

    some test tidyup

--- RockScissorsPaper/WeaponOfMassDestructionTest.cs
+++ RockScissorsPaper/WeaponOfMassDestructionTest.cs
@@ -6,57 +6,27 @@ namespace RockScissorsPaper
     public class WeaponOfMassDestructionTest
     {
         [Test]
-        public void PaperShouldBeatRock()
+        public void PaperVersusTheRest()
         {
             Assert.AreEqual(true, new Paper().BeatsRock());
-        }
-
-        [Test]
-        public void PaperShouldNotBeatItself()
-        {
             Assert.AreEqual(false, new Paper().BeatsPaper());
-        }
-
-        [Test]
-        public void PaperShouldNotBeatScissors()
-        {
             Assert.AreEqual(false, new Paper().BeatsScissors());
         }
 
         [Test]
-        public void RockShouldNotBeatPaper()
+        public void RockVersusTheRest()
         {
             Assert.AreEqual(false, new Rock().BeatsPaper());
-        }
-
-        [Test]
-        public void RockShouldNotBeatItself()
-        {
             Assert.AreEqual(false, new Rock().BeatsRock());
-        }
-
-        [Test]
-        public void RockShouldBeatScissors()
-        {
             Assert.AreEqual(true, new Rock().BeatsScissors());
         }
 
         [Test]
-        public void ScissorsShouldNotBeatItself()
+        public void ScissorsVersusTheRest()
         {
             Assert.AreEqual(false, new Scissors().BeatsScissors());
-        }
-
-        [Test]
-        public void ScissorsShouldNotBeatRock()
-        {
             Assert.AreEqual(false, new Scissors().BeatsRock());
-        }
-
-        [Test]
-        public void ScissorsShouldBeatPaper()
-        {
             Assert.AreEqual(true, new Scissors().BeatsPaper());
-        }     
+        }   
     }
 }
