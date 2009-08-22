16c9e8351029ea60f81b84f3fa738a1cbc5a17de
unknown <Administrator@.(none)>
2009-08-22 01:57:37 +0100

    battle result deserves its on place in this world. can't live off battle forever

--- RockScissorsPaper/Battle.cs
+++ RockScissorsPaper/Battle.cs
@@ -24,34 +24,4 @@
             return winnerDetermination.Winner();
         }
     }
-
-    public class BattleResult
-    {
-        private readonly IWeapon winner;
-
-        private BattleResult(IWeapon winner)
-        {
-            this.winner = winner;
-        }
-
-        public IWeapon Winner
-        {
-            get { return winner; }
-        }
-
-        public bool HasWinner
-        {
-            get { return winner != null; }
-        }
-
-        public static BattleResult WithWinner(IWeapon winner)
-        {
-            return new BattleResult(winner);
-        }
-
-        public static BattleResult WithNoWinner()
-        {
-            return new BattleResult(null);
-        }
-    }
 }
